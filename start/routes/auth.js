970e06c19615a44fa45eb9629822d840cf9630ba
Marcos Vinicius <marvn.ants1@gmail.com>
2019-12-06 12:07:12 -0300

    authentication routes

--- start/routes/auth.js
+++ start/routes/auth.js
@@ -12,6 +12,12 @@ Route.group(() => {
     Route.post('/login', 'AuthController.login').as('auth.login')
     Route.post('/refresh', 'AuthController.refresh').as('auth.resfresh')
     Route.post('/logout', 'AuthController.logout').as('auth.logout')
+    
+    // restore password routes
+
+    Route.post('reset-password', 'AuthController.forgot').as('auth.forgot')
+    Route.get('reset-password', 'AuthController.remember').as('auth.remember')
+    Route.put('reset-password', 'AuthController.reset').as('auth.reset')
 })
     .prefix('v1/auth')
     .namespace('Auth')
